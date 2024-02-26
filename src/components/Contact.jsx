d32336bb0752e969b249d17d565672ad87dfcfcd
GitforJosh375 <joshualilly14@gmail.com>
2024-02-25 21:59:10 -0600

    Final commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,10 @@ function ContactUs () {
             <div className="mission-text">
             <h1>Contact Information</h1>
             <h2>Church</h2>
-            <p><b>Phone:</b> (573) 674-3141 <br></br><b>E-mail:</b> fbc_licking@mail.com<br></br><b>Address:</b> 223 South Main Street PO Box 216 Licking, MO 65542</p>
+            <p><b>Phone:</b><br></br> (573) 674-3141 <br></br><b>E-mail:</b> fbc_licking@mail.com<br></br><b>Address:</b><br></br> 223 South Main Street PO Box 216 Licking, MO 65542</p>
             
             <h2>Pastor Rob Lilly</h2>
-            <p><b>Phone:</b> (573) 889-8556 </p>
+            <p><b>Phone:</b><br></br> (573) 889-8556 </p>
             </div>
             <div className="mission-img">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12662.164269980327!2d-91.86792794440287!3d37.49515497763319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87da884cbe2f0873%3A0xc17a5deae87d1dbb!2sFirst%20Baptist%20Church!5e0!3m2!1sen!2sus!4v1708837115974!5m2!1sen!2sus" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
